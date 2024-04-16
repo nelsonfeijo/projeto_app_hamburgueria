99144fb3fb1100e3a4f06bcbcd57430e1449f142
Nelson Feijó <114948173+nelsonfeijo@users.noreply.github.com>
2024-04-16 17:44:05 -0300

    Update script.js

--- script.js
+++ script.js
@@ -39,10 +39,29 @@ function addToCart(name, price){
 
     if(existingItem){
         existingItem.quantity += 1
+    } else {
+        cart.push({
+            name,
+            price,
+            quantity: 1,
+        })
     }
-    cart.push({
-        name,
-        price,
-        quantity: 1,
+    updateCartModal()
+}
+
+function updateCartModal(){
+    cartItemsContainer.innerHTML = ""
+    let total = 0;
+
+    cart.forEach(item => {
+        const cartItemElement = document.createElement("div");
+        cartItemElement.innerHTML = `
+        <div>
+        <div>
+        <p>${item.name}</p>
+        <p>${item.price}</p>
+        </div>
+        </div>
+        `
     })
 }
